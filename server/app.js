05cb880d638385a3a226c15b52341be95fb6fcb0
alexander1fein <feinberg.alexander@gmail.com>
2022-09-06 18:14:02 -0400

    Done

--- server/app.js
+++ server/app.js
@@ -23,8 +23,32 @@ const {
 
 const express = require('express');
 const app = express();
+app.use(express.json())
+app.use((req, res, next) =>{
+  console.log("Body: ", req.body)
+  next();
+})
 
 // Your code here
+app.get("/artists", (req, res)=>{
+  res.send(getAllArtists())
+})
+
+app.post('/artists', (req, res)=>{
+  const added = addArtist(req.body)
+  res.status(201)
+  res.send(added)
+
+
+})
+
+app.get('/artists/latest', (req, res)=>{
+  res.send(getLatestArtist())
+})
+
+app.get('/artists/latest/albums', (req, res)=>{
+  res.send(getAlbumsForLatestArtist())
+})
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
